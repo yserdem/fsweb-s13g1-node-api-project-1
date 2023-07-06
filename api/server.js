c9c440e0d6f0e9c7c1bb8c3dcb5608b475a8e227
Sinan <y.sinanerdem@gmail.com>
2023-07-06 14:38:43 +0300

    MVP complete, all tests passed

--- api/server.js
+++ api/server.js
@@ -1,3 +1,79 @@
 // SUNUCUYU BU DOSYAYA KURUN
+const express = require("express");
+const server = express();
 
-module.exports = {}; // SERVERINIZI EXPORT EDİN {}
+const userModel = require("./users/model");
+server.use(express.json());
+
+server.post("/api/users", async (req, res) => {
+    try {
+        const { name, bio } = req.body;
+        if (!name || !bio) {
+            res.status(400).json({ message: "Lütfen kullanıcı için bir name ve bio sağlayın" });
+        } else {
+            const inserted = await userModel.insert({ name: name, bio: bio });
+            res.status(201).json(inserted)
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Veritabanına kaydedilirken bir hata oluştu" })
+    }
+});
+
+server.get("/api/users/", async (req, res) => {
+    try {
+        const allUsers = await userModel.find();
+        res.json(allUsers);
+    } catch (error) {
+        res.status(500).json({ message: "Kullanıcı bilgileri alınamadı" })
+    }
+});
+
+server.get(`/api/users/:id`, async(req, res) => {
+    try {
+        const user = await userModel.findById(req.params.id);
+        if (!user) {
+            res.status(404).json({ message: "Belirtilen ID'li kullanıcı bulunamadı" })
+        } else {
+            res.json(user);
+        }
+        
+    } catch (error) {
+        res.status(500).json({ message: "Kullanıcı bilgisi alınamadı" })
+    }
+})
+
+server.delete(`/api/users/:id`, async(req, res) => {
+    try {
+        const user = await userModel.findById(req.params.id);
+        if (!user) {
+            res.status(404).json({ message: "Belirtilen ID'li kullanıcı bulunamadı" })
+        } else {
+            const deleted = await userModel.remove(req.params.id)
+            res.json(deleted)
+        }
+        
+    } catch (error) {
+        res.status(500).json({ message: "Kullanıcı silinemedi" })
+    }
+})
+
+server.put(`/api/users/:id`, async(req, res) => {
+    try {
+        const user = await userModel.findById(req.params.id);
+        const {name, bio} = req.body;
+        if (!user) {
+            res.status(404).json({ message: "Belirtilen ID'li kullanıcı bulunamadı" })
+        } else if (!name || !bio) {
+            res.status(400).json({ message: "Lütfen kullanıcı için name ve bio sağlayın" });
+        } else if (user && name && bio) {
+            const updated = await userModel.update(req.params.id, {name: name, bio: bio})
+            res.json(updated)
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Kullanıcı bilgileri güncellenemedi" })
+    }
+})
+
+
+
+module.exports = server; // SERVERINIZI EXPORT EDİN {}
--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const server = require('./api/server');
 
 const port = 9000;
 
+server.listen(port, ()=> {
+    console.log(`Server is listening on ` + port)
+})
+
 // START YOUR SERVER HERE
